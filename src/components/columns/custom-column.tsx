de282b6e1338b90df4d15c7aa2b3c242dc55a558
candrews <candrews@vectorform.com>
2020-04-03 20:46:46 -0400

    improve custom column handles

--- src/components/columns/custom-column.tsx
+++ src/components/columns/custom-column.tsx
@@ -3,14 +3,13 @@ import { withHeader } from './with-header';
 import { TableCell } from '../elements';
 import { FlexTable } from '../..';
 
-
-export const CustomColumn = withHeader<FlexTable.CustomColumnProps<unknown>>(({ item, onRender, children, onAction, cellClassName, hideHeader, ...attrs }) => {
+export const CustomColumn = withHeader(<T extends {}>({ item, onRender, children, onAction, cellClassName, hideHeader, ...attrs }: FlexTable.CustomColumnProps<T>) => {
     const className = (cellClassName && typeof cellClassName === 'function') ? cellClassName(item) : cellClassName;
     const onClickTableCell: React.MouseEventHandler<HTMLDivElement> = event => 
         (onAction && onAction(item, event));
     return (
         <TableCell onClick={onClickTableCell} cellClassName={className} { ...attrs }>
-            {onRender(item)}
+            {item && onRender(item)}
         </TableCell>
     )
 });
